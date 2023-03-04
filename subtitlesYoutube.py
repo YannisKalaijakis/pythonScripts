7a33316b6e333db942dcc7ee79c9b3f2cdb66e45
YannisKalaijakis <yannis.kalaijakis@gmail.com>
2023-03-04 23:30:30 +0200

    download subtitles from a youtube video

---
+++ subtitlesYoutube.py
@@ -0,0 +1,12 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import nltk
+nltk.download('punkt')
+
+srt = YouTubeTranscriptApi.get_transcript('jQVCSKpQmCo', languages=['en'])
+
+with open("subtitles_eliamep.txt", "w") as f:
+   
+
+    for i in srt:
+
+        f.write("{}\n".format(i['text']))
